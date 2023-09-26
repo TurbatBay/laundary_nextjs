495000337c1b0623f69df4c7c86ab92d73695a89
TurbatBay <learnwithturu@gmail.com>
2023-09-26 21:26:36 +0800

    Initial Commit Hello

---
+++ fe/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className='min-h-screen  p-24'>
+      <div>
+        <h1>Hello World</h1>
+      </div>
+    </main>
+  )
+}
